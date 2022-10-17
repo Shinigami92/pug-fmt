7eaf908813668c388a9b952337f7b0d0b3264e35
Shinigami92 <chrissi92@hotmail.de>
2022-10-17 16:15:04 +0200

    infra: build with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,17 @@
 /// <reference types="vitest" />
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'pug-fmt',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    target: ['es2019', 'node14.6'],
+  },
+  plugins: [dts()],
+});
